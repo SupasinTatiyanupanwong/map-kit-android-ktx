7e85bb20a60d2972a58cb1a99d0a48043287cf70
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-06 16:49:12 +0700

    Project setup

---
+++ maps-ktx/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 19
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.maps.core)
+}
