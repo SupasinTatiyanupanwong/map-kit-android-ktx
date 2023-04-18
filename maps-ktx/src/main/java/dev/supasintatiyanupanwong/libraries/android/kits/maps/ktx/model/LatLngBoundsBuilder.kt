e0d2b9527f1cd571f0b023e6c735491e0dae7a07
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-18 15:26:09 +0700

    Cleaned up import statements for LatLngBoundsBuilder

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLngBoundsBuilder.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLngBoundsBuilder.kt
@@ -3,7 +3,6 @@
 package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
-import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLng
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLngBounds
 
 inline fun LatLngBoundsBuilder(): LatLngBounds.Builder = MapKit.newLatLngBoundsBuilder()
