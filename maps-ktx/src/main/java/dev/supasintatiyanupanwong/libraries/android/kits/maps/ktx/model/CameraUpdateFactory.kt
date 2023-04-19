3e41d70aa42ce0de839b3368816248aac25806d7
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-19 15:21:06 +0700

    Added KTX for CameraUpdateFactory

---
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/CameraUpdateFactory.kt
@@ -0,0 +1,6 @@
+package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.CameraUpdate
+
+object CameraUpdateFactory : CameraUpdate.Factory by MapKit.getCameraUpdateFactory()
