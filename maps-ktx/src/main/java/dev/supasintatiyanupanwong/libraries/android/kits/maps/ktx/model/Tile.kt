cb95222964ae26c9a287b2af6a504b90cc58b9fb
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-20 13:16:24 +0700

    Added KTX for Tile

---
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/Tile.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Tile
+
+inline fun Tile(width: Int, height: Int, data: ByteArray?): Tile = MapKit.newTile(width, height, data)
