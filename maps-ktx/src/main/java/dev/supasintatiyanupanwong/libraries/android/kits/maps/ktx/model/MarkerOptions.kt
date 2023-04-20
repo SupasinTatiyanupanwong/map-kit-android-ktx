223c946ce64eedaab1f84022cba597c63464820c
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-20 20:23:13 +0700

    Added KTX for MarkerOptions

---
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/MarkerOptions.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Marker
+
+inline fun MarkerOptions(): Marker.Options = MapKit.newMarkerOptions()
