c0f759851633009938db28db48040247d42a7daa
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-20 20:19:57 +0700

    Added KTX for TileOverlayOptions

---
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/TileOverlayOptions.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.TileOverlay
+
+inline fun TileOverlayOptions(): TileOverlay.Options = MapKit.newTileOverlayOptions()
