2ebc388e2f422cdd61272d6d1f4456b60493908b
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-04-24 11:19:36 +0700

    Added KTX for Gap

---
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/Gap.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Gap
+
+inline fun Gap(length: Float): Gap = MapKit.newGap(length)
