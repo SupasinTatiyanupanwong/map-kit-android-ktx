f42b60565e4f30dc22f8d6044b16b2a6a5773159
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-09-25 21:20:00 +0700

    Implemented destructuring declarations for `LatLng`

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLng.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLng.kt
@@ -6,3 +6,23 @@ import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLng
 
 inline fun LatLng(latitude: Double, longitude: Double): LatLng = MapKit.newLatLng(latitude, longitude)
+
+/**
+ * Returns the [LatLng.latitude][LatLng.getLatitude] of this [LatLng].
+ *
+ * e.g.
+ * ```
+ * val (lat, _) = latLng
+ * ```
+ */
+inline operator fun LatLng.component1(): Double = this.latitude
+
+/**
+ * Returns the [LatLng.longitude][LatLng.getLongitude] of this [LatLng].
+ *
+ * e.g.
+ * ```
+ * val (_, lng) = latLng
+ * ```
+ */
+inline operator fun LatLng.component2(): Double = this.longitude
