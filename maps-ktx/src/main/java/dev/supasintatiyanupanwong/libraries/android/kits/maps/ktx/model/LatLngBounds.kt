ab0c57b599607a416bd4c62b5d42041d8c9f6a36
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-21 17:55:17 +0700

    Reworked KTX for LatLngBoundsBuilder

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLngBounds.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLngBounds.kt
@@ -6,4 +6,10 @@ import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLng
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLngBounds
 
-inline fun LatLngBounds(southwest: LatLng, northeast: LatLng): LatLngBounds = MapKit.newLatLngBounds(southwest, northeast)
+inline fun LatLngBounds(southwest: LatLng, northeast: LatLng): LatLngBounds {
+    return MapKit.newLatLngBounds(southwest, northeast)
+}
+
+inline fun LatLngBounds(actions: LatLngBounds.Builder.() -> Unit): LatLngBounds {
+    return MapKit.newLatLngBoundsBuilder().apply(actions).build()
+}
--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/LatLngBoundsBuilder.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
-package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
-
-import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
-import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLngBounds
-
-@Suppress("FunctionName")
-inline fun LatLngBoundsBuilder(): LatLngBounds.Builder = MapKit.newLatLngBoundsBuilder()
