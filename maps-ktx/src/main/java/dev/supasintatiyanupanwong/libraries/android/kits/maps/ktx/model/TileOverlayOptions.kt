0179e7598804bd6ff87afab7df352ffde79e63e7
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:15:03 +0700

    Added KTX for TileOverlayOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/TileOverlayOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/TileOverlayOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.TileOverlay
 
+/**
+ * Builds a new [TileOverlay.Options].
+ *
+ * @return the constructed [TileOverlay.Options].
+ */
 @Suppress("FunctionName")
-inline fun TileOverlayOptions(): TileOverlay.Options = MapKit.newTileOverlayOptions()
+inline fun TileOverlayOptions(): TileOverlay.Options {
+    return MapKit.newTileOverlayOptions()
+}
+
+/**
+ * Builds a new [TileOverlay.Options] using the provided [actions].
+ *
+ * @return the constructed [TileOverlay.Options].
+ */
+@Suppress("FunctionName")
+inline fun TileOverlayOptions(actions: TileOverlay.Options.() -> Unit): TileOverlay.Options {
+    return TileOverlayOptions().apply(actions)
+}
