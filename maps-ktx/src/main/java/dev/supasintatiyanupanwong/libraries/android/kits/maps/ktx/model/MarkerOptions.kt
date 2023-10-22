153b2c894f4deef8e37f98f0f2b1ca68ca053670
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:12:08 +0700

    Added KTX for MarkerOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/MarkerOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/MarkerOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Marker
 
+/**
+ * Builds a new [Marker.Options].
+ *
+ * @return the constructed [Marker.Options].
+ */
 @Suppress("FunctionName")
-inline fun MarkerOptions(): Marker.Options = MapKit.newMarkerOptions()
+inline fun MarkerOptions(): Marker.Options {
+    return MapKit.newMarkerOptions()
+}
+
+/**
+ * Builds a new [Marker.Options] using the provided [actions].
+ *
+ * @return the constructed [Marker.Options].
+ */
+@Suppress("FunctionName")
+inline fun MarkerOptions(actions: Marker.Options.() -> Unit): Marker.Options {
+    return MarkerOptions().apply(actions)
+}
