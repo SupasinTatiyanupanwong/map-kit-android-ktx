3f9a6f5db0127f8b2cf07cdca04dd8c68a415ac7
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:13:19 +0700

    Added KTX for PolygonOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/PolygonOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/PolygonOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Polygon
 
+/**
+ * Builds a new [Polygon.Options].
+ *
+ * @return the constructed [Polygon.Options].
+ */
 @Suppress("FunctionName")
-inline fun PolygonOptions(): Polygon.Options = MapKit.newPolygonOptions()
+inline fun PolygonOptions(): Polygon.Options {
+    return MapKit.newPolygonOptions()
+}
+
+/**
+ * Builds a new [Polygon.Options] using the provided [actions].
+ *
+ * @return the constructed [Polygon.Options].
+ */
+@Suppress("FunctionName")
+inline fun PolygonOptions(actions: Polygon.Options.() -> Unit): Polygon.Options {
+    return PolygonOptions().apply(actions)
+}
