da9a2417dada138d1706f59d71d4fbc071d1dbc5
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:11:15 +0700

    Added KTX for GroundOverlayOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/GroundOverlayOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/GroundOverlayOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.GroundOverlay
 
+/**
+ * Builds a new [GroundOverlay.Options].
+ *
+ * @return the constructed [GroundOverlay.Options].
+ */
 @Suppress("FunctionName")
-inline fun GroundOverlayOptions(): GroundOverlay.Options = MapKit.newGroundOverlayOptions()
+inline fun GroundOverlayOptions(): GroundOverlay.Options {
+    return MapKit.newGroundOverlayOptions()
+}
+
+/**
+ * Builds a new [GroundOverlay.Options] using the provided [actions].
+ *
+ * @return the constructed [GroundOverlay.Options].
+ */
+@Suppress("FunctionName")
+inline fun GroundOverlayOptions(actions: GroundOverlay.Options.() -> Unit): GroundOverlay.Options {
+    return GroundOverlayOptions().apply(actions)
+}
