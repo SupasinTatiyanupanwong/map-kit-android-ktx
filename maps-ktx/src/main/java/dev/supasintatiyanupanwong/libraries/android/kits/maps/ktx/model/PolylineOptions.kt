05f2bf8251b0af3c9e930dac05d42cf0a605dc7b
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:14:06 +0700

    Added KTX for PolylineOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/PolylineOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/PolylineOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Polyline
 
+/**
+ * Builds a new [Polyline.Options].
+ *
+ * @return the constructed [Polyline.Options].
+ */
 @Suppress("FunctionName")
-inline fun PolylineOptions(): Polyline.Options = MapKit.newPolylineOptions()
+inline fun PolylineOptions(): Polyline.Options {
+    return MapKit.newPolylineOptions()
+}
+
+/**
+ * Builds a new [Polyline.Options] using the provided [actions].
+ *
+ * @return the constructed [Polyline.Options].
+ */
+@Suppress("FunctionName")
+inline fun PolylineOptions(actions: Polyline.Options.() -> Unit): Polyline.Options {
+    return PolylineOptions().apply(actions)
+}
