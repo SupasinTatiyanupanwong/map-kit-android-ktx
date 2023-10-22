7041ae29b7e882764365a2077160bc4634c43fe8
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-10-22 21:10:34 +0700

    Added KTX for CircleOptions with function literal

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/CircleOptions.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/CircleOptions.kt
@@ -5,5 +5,22 @@ package dev.supasintatiyanupanwong.libraries.android.kits.maps.ktx.model
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Circle
 
+/**
+ * Builds a new [Circle.Options].
+ *
+ * @return the constructed [Circle.Options].
+ */
 @Suppress("FunctionName")
-inline fun CircleOptions(): Circle.Options = MapKit.newCircleOptions()
+inline fun CircleOptions(): Circle.Options {
+    return MapKit.newCircleOptions()
+}
+
+/**
+ * Builds a new [Circle.Options] using the provided [actions].
+ *
+ * @return the constructed [Circle.Options].
+ */
+@Suppress("FunctionName")
+inline fun CircleOptions(actions: Circle.Options.() -> Unit): Circle.Options {
+    return CircleOptions().apply(actions)
+}
