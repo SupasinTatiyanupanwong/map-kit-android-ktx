96aa42b250c9c8ea2b20cd04886c392a7955532a
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-11-03 23:13:01 +0700

    Cleaned up KTX documentation for map drawing

--- maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/MapClient.kt
+++ maps-ktx/src/main/java/dev/supasintatiyanupanwong/libraries/android/kits/maps/ktx/model/MapClient.kt
@@ -9,54 +9,60 @@ import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Polyline
 import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.TileOverlay
 
 /**
- * Adds a [Circle] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [Circle] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the circle.
  *
- * @return The [Circle] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [Circle.Options] object.
  */
 inline fun MapClient.addCircle(actions: Circle.Options.() -> Unit): Circle? {
     return addCircle(CircleOptions(actions))
 }
 
 /**
- * Adds a [GroundOverlay] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [GroundOverlay] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the overlay.
  *
- * @return the [GroundOverlay] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [GroundOverlay.Options] object.
  */
 inline fun MapClient.addGroundOverlay(actions: GroundOverlay.Options.() -> Unit): GroundOverlay? {
     return addGroundOverlay(GroundOverlayOptions(actions))
 }
 
 /**
- * Adds a [Marker] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [Marker] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the marker.
  *
- * @return the [Marker] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [Marker.Options] object.
  */
 inline fun MapClient.addMarker(actions: Marker.Options.() -> Unit): Marker? {
     return addMarker(MarkerOptions(actions))
 }
 
 /**
- * Adds a [Polygon] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [Polygon] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the polygon.
  *
- * @return the [Polygon] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [Polygon.Options] object.
  */
 inline fun MapClient.addPolygon(actions: Polygon.Options.() -> Unit): Polygon? {
     return addPolygon(PolygonOptions(actions))
 }
 
 /**
- * Adds a [Polyline] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [Polyline] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the polyline.
  *
- * @return the [Polyline] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [Polyline.Options] object.
  */
 inline fun MapClient.addPolyline(actions: Polyline.Options.() -> Unit): Polyline? {
     return addPolyline(PolylineOptions(actions))
 }
 
 /**
- * Adds a [TileOverlay] to this [MapClient] using the function literal with receiver [actions].
+ * Returns the [TileOverlay] object that is added to this [MapClient] using the function literal
+ * with receiver [actions]. Might be `null` if there's an error adding the overlay.
  *
- * @return the [TileOverlay] object that is added to the map. Might be `null` if there's an error.
+ * Using this removes the need to explicitly construct a [TileOverlay.Options] object.
  */
 inline fun MapClient.addTileOverlay(actions: TileOverlay.Options.() -> Unit): TileOverlay? {
     return addTileOverlay(TileOverlayOptions(actions))
